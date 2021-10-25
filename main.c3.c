d29189fea64e0c3e213e605daa8adf122de48013
sanchellaryogola <91962458+sanchellaryogola@users.noreply.github.com>
2021-10-25 17:22:44 +0300

    Add files via upload

---
+++ main.c3.c
@@ -0,0 +1,26 @@
+/*
+Project:Library system
+Author:Hillary Ogola
+Date:Oct,2021
+Language:C99
+Compiler:GCC
+License:MIT
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("Hello world!\n");
+    printf("Add book\n");
+    printf("Add title\n");
+    printf("Add user\n");
+    printf("Add search\n");
+    printf("Add download\n");
+    printf("Add comment\n");
+    printf("exit\n");
+    return 0;
+}
